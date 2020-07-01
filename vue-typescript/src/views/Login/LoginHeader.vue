7ea4442d9449fcb89a141c2e3865b70baa7abdbc
18292982664 <36914185+18292982664@users.noreply.github.com>
2020-07-01 10:03:30 +0800

    添加登录页面

---
+++ vue-typescript/src/views/Login/LoginHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="login-header">
+        <div class="header">login-登录</div>
+        <div class="container">
+            <slot name="container"></slot>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+    components: {}
+})
+export default class Layout extends Vue {}
+</script>
+<style lang="scss">
+.header {
+    text-align: center;
+}
+</style>
---
+++ vue-typescript/src/views/Login/Password.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="password">
+        <LoginHeader>
+            <el-from slot="container">
+                <div class="title">
+                    <h3>找回密码</h3>
+                </div>
+            </el-from>
+        </LoginHeader>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import LoginHeader from "./LoginHeader.vue";
+@Component({
+    components: { LoginHeader }
+})
+export default class Password extends Vue {}
+</script>
+<style lang="scss">
+.title {
+    text-align: center;
+    font-size: 16px;
+    width: 500px;
+    height: 400px;
+    border: 1px solid wheat;
+    margin: 0 auto;
+}
+</style>
